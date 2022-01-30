214dc5ffa2049168b5b9c65cdabf115c3d0383da
Vasa300 <zhevlakovyi@gmail.com>
2022-01-30 11:05:15 +0200

    new class

---
+++ test/src/com/test/Square.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class Square extends Quadrilateral {
+    public void display(){
+        System.out.println("Square "+id);
+    }
+}
--- test/src/com/test/test.java
+++ test/src/com/test/test.java
@@ -2,6 +2,11 @@ package com.test;
 
 public class test {
     public static void main(String[] args) {
-
+    Figure trR = new TriangleRavnobedren();
+    trR.display();
+    Figure o = new Oval();
+    o.display();
+    Triange tr = new TrianglePryam();
+    tr.display();
     }
 }
